b7fb428a2016bf8e866a88eacacf093c70f08300
Matthew Dulworth <mhdulworth@gmail.com>
2019-10-08 20:29:32 -0500

    aadded support to z-rotation for FOV

--- Assets/Scripts/Main/FieldOfView.cs
+++ Assets/Scripts/Main/FieldOfView.cs
@@ -26,16 +26,16 @@ public class FieldOfView : MonoBehaviour
    // Public Methods
    // ------------------------------------------------------
    public Vector2 GetDirectionVector(){
-      float angle = direction * Mathf.Deg2Rad;
+      float angle = (direction - transform.eulerAngles.z) * Mathf.Deg2Rad;
       return new Vector2(viewRadius * Mathf.Sin(angle), viewRadius * Mathf.Cos(angle));
    }
 
    public Vector2 GetVectorA(){
-      float angle = (viewAngle + direction * 2) * Mathf.Deg2Rad / 2.0f;
+      float angle = (viewAngle + direction * 2 - transform.eulerAngles.z * 2) * Mathf.Deg2Rad / 2.0f;
       return new Vector2(viewRadius * Mathf.Sin(angle), viewRadius * Mathf.Cos(angle));
    }
    public Vector2 GetVectorB(){
-      float angle = (viewAngle - direction * 2) * Mathf.Deg2Rad / 2.0f;
+      float angle = (viewAngle - direction * 2 + transform.eulerAngles.z * 2) * Mathf.Deg2Rad / 2.0f;
       return new Vector2(-viewRadius * Mathf.Sin(angle), viewRadius * Mathf.Cos(angle));
    }
 
