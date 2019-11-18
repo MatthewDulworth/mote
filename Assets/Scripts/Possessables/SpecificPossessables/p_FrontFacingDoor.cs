3bad562afcbdf9ee40e3b32651082f8fa546f46e
Matthew Dulworth <mhdulworth@gmail.com>
2019-11-18 13:12:22 -0600

    Added method to handle dad movement to dad room controller

---
+++ Assets/Scripts/Possessables/SpecificPossessables/p_FrontFacingDoor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class p_FrontFacingDoor : Possessable
+{
+   public override void OnUpdate(InputController io)
+   {
+
+   }
+
+   public override void OnFixedUpdate(InputController io)
+   {
+
+   }
+}
--- Assets/Scripts/SceneSpecificControllers/DadRoomController.cs
+++ Assets/Scripts/SceneSpecificControllers/DadRoomController.cs
@@ -6,28 +6,37 @@ public class DadRoomController : SceneSpecificController
 {
    [SerializeField] private GameObject groundEnemyPrefab;
    [SerializeField] private GameObject enemyWallPrefab;
+   [SerializeField] private GameObject drunkDad;
    [SerializeField] private Vector3 enemy1SpawnPosition;
    [SerializeField] private Vector3 enemy2SpawnPosition;
    [SerializeField] private Vector3 wallSpawnPosition;
 
    private Enemy enemy1;
    private Enemy enemy2;
+   private p_FrontFacingDoor exit;
+
+   private bool dadMoveTrigger = false;
 
    public override void OnStart(EnemyController enemyControl)
    {
-
+      exit = FindObjectOfType<p_FrontFacingDoor>();
    }
 
    public override void OnUpdate(EnemyController enemyControl)
    {
-      if(Input.GetKeyDown(KeyCode.Space)){
-         BeginEnemyEncounter(enemyControl);
+      // Debugging
+      if (Input.GetKeyDown(KeyCode.Space))
+      {
+         // BeginEnemyEncounter(enemyControl);
+         dadMoveTrigger = true;
       }
-      if(Input.GetKeyDown(KeyCode.LeftCommand)){
-         Debug.Log("yeet");
-         enemyControl.DestroyEnemy(enemy1);
-         enemyControl.DestroyEnemy(enemy2);
+      if (Input.GetKeyDown(KeyCode.LeftCommand))
+      {
+         // enemyControl.DestroyEnemy(enemy1);
+         // enemyControl.DestroyEnemy(enemy2);
       }
+
+      HandleDadMovement();
    }
 
    private void BeginEnemyEncounter(EnemyController enemyControl)
@@ -40,5 +49,17 @@ public class DadRoomController : SceneSpecificController
       list.Add(enemy2);
 
       enemyControl.SpawnEnemyWall(enemyWallPrefab, list, wallSpawnPosition, new Vector3(2, 29, 1));
+   }
+
+   private void HandleDadMovement(){
+      if (dadMoveTrigger == true && drunkDad != null)
+      {
+         Vector3 target = new Vector3(exit.transform.position.x, drunkDad.transform.position.y, 0);
+         drunkDad.transform.position = Vector2.MoveTowards(drunkDad.transform.position, target, 3*Time.deltaTime);
+
+         if(drunkDad.transform.position == target){
+            Destroy(drunkDad);
+         }
+      }
    }
 }
