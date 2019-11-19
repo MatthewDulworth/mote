a78bc825ca99e91569086dfaaba7dff5e89e89b5
Matthew Dulworth <mhdulworth@gmail.com>
2019-11-18 20:52:23 -0600

    Added collision detection to hitbox

--- Assets/Scripts/Add-Ons/HitBox.cs
+++ Assets/Scripts/Add-Ons/HitBox.cs
@@ -2,15 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public class HitBox : MonoBehaviour
 {
+   // ------------------------------------------------------
+   // Member Vars
+   // ------------------------------------------------------
    private new BoxCollider2D collider;
+   private List<string> tags;
+   private List<GameObject> objectsColliding;
 
-   void Start(){
+   // ------------------------------------------------------
+   // Mono Methods
+   // ------------------------------------------------------
+   void Start()
+   {
       collider = GetComponent<BoxCollider2D>();
+      tags = new List<string>();
+      objectsColliding = new List<GameObject>();
+
+      if (!collider.isTrigger)
+      {
+         collider.isTrigger = true;
+      }
    }
 
-   public BoxCollider2D Collider{
-      get{return collider;}
+   // ------------------------------------------------------
+   // Collisions
+   // ------------------------------------------------------
+   public void OnTriggerEnter2D(Collider2D collider)
+   {
+      foreach(string tag in tags){
+         if(tag == collider.tag){
+            objectsColliding.Add(collider.gameObject);
+         }
+      }
+   }
+
+   public void OnTriggerExit2D(Collider2D collider)
+   {
+      objectsColliding.Remove(collider.gameObject);
+   }
+
+   // ------------------------------------------------------
+   // Getters/Setters
+   // ------------------------------------------------------
+   public void SetCollisionTags(List<string> tags)
+   {
+      this.tags = tags;
+   }
+
+   public bool IsColliding()
+   {
+     return (objectsColliding.Count > 0);
+   }
+
+   public List<GameObject> ObjectsColliding
+   {
+      get { return objectsColliding; }
+   }
+
+   public BoxCollider2D Collider
+   {
+      get { return collider; }
    }
 }
