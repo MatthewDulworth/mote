d49ab52a7ec1ffb0b41ddff2554b033091d31248
Matthew Dulworth <mhdulworth@gmail.com>
2019-11-25 19:55:22 -0600

    Added ProjectileArc aand TrajectorySimulation classess, they techinically work, but they use janky magic numbers so they need to be rewritten

---
+++ Assets/Scripts/Add-Ons/ProjectileArc.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ProjectileArc
+{
+   // ------------------------------------------------------
+   // Member Vars
+   // ------------------------------------------------------
+   public float Gravity { get; private set; }         // the gravity being used
+   public float Mass { get; private set; }            // the mass of the projectile
+   public float InitialHeight { get; private set; }   // intitial height above the ground
+
+   public float Velocity { get; private set; }        // velocity 
+   public float VelocityX { get; private set; }       // x component of velocity 
+   public float VelocityY { get; private set; }       // y componenet fo velocity 
+   public float Angle { get; private set; }           // angle in radians relative to the forwards unit vector
+
+   public float Range { get; private set; }           // the maximum x displacement of the projectile
+   public float TimeOfFlight { get; private set; }    // time rquired for the projectile 
+   public float MaxHeight { get; private set; }       // highest point the projectile reaches
+
+
+   // ------------------------------------------------------
+   // Constructor
+   // ------------------------------------------------------
+   public ProjectileArc(Vector2 force, float gravity, float mass, float height)
+   { 
+      Gravity = gravity;
+      Mass = mass;
+      InitialHeight = height;
+
+      Velocity = force.magnitude * mass;
+      VelocityX = force.x * mass;
+      VelocityY = force.y * mass;
+      Angle = Mathf.Atan2(force.y, force.x);
+      
+      Range = VelocityX * (VelocityY + Mathf.Sqrt(VelocityY * VelocityY + 2 * gravity * InitialHeight)) / gravity;
+      TimeOfFlight = (VelocityY + Mathf.Sqrt(VelocityY * VelocityY + 2 * gravity * InitialHeight)) / gravity;
+      MaxHeight = InitialHeight + VelocityY * VelocityY / (2 * gravity);
+   }
+
+
+   // ------------------------------------------------------
+   // Position
+   // ------------------------------------------------------
+   public Vector2 Position(float time)
+   {
+      float x = VelocityX * time;
+      float y = InitialHeight + VelocityY * time - Gravity * time * time / 2.0f;
+
+      return new Vector2(x, y);
+   }
+}
---
+++ Assets/Scripts/Add-Ons/TrajectorySimulator.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrajectorySimulator : MonoBehaviour
+{
+   // ------------------------------------------------------
+   // Member Vars
+   // ------------------------------------------------------
+   [SerializeField] private int resolution;           // the amount of line segemnts the arc should be drawn with
+   [SerializeField] private float maxFallDistance;    // 
+
+   private LineRenderer lineRenderer;
+   private int positionsCount;
+   private float mass;
+   private float gravity;
+
+
+   // ------------------------------------------------------
+   // Starts
+   // ------------------------------------------------------
+   public void OnValidate()
+   {
+      resolution = Mathf.Max(0, resolution);
+      positionsCount = resolution + 1;
+   }
+
+   public void Awake()
+   {
+      lineRenderer = GetComponent<LineRenderer>();
+      mass = GetComponent<Rigidbody2D>().mass;
+      gravity = Physics2D.gravity.y;
+   }
+
+   // ------------------------------------------------------
+   // Simulate Trajectory
+   // ------------------------------------------------------
+   public void SimulateTrjectory(Vector2 force)
+   {
+      ProjectileArc arc = new ProjectileArc(force, gravity, mass, maxFallDistance);
+      Vector3[] positions = new Vector3[positionsCount + 1];
+
+      float timeStep = arc.TimeOfFlight / (float)positionsCount;
+
+      for (int i = 0; i <= positionsCount; i++)
+      {
+         positions[i] = -arc.Position(i * timeStep) + (Vector2)transform.position;
+      }
+
+      RenderTrajectory(positions);
+   }
+
+   // ------------------------------------------------------
+   // Render Trajectory
+   // ------------------------------------------------------
+   public void RenderTrajectory(Vector3[] positions)
+   {
+      lineRenderer.positionCount = positionsCount + 1;
+      lineRenderer.SetPositions(positions);
+   }
+
+   public void Update()
+   {
+      if (Input.GetKeyDown(KeyCode.Space))
+      {
+         Vector2 impulse = new Vector2(5, 10);
+         Debug.LogFormat("Start: {0}", (Vector2)transform.position);
+         SimulateTrjectory(impulse);
+         this.GetComponent<Rigidbody2D>().AddForce(impulse, ForceMode2D.Impulse);
+      }
+   }
+}
